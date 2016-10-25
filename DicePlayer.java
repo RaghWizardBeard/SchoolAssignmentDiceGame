8b1a9547d93ad5f2bc792ea9754bd66941fe38c2
RaghWizardBeard <emersonemporium7@gmail.com>
2016-10-25 10:01:27 -0400

    Added constructor

--- DicePlayer.java
+++ DicePlayer.java
@@ -12,10 +12,14 @@
 
 
 public class DicePlayer {
-
     String playerName;
     double playerCash;
     
+    public DicePlayer(String name, double cash)
+    {
+    playerName = name;
+    playerCash= cash;
+    }
     public void setName(String requestedName)
     {
         playerName = requestedName;
@@ -61,70 +65,8 @@ public class DicePlayer {
     return depositSlip;
     }
     
-    public static void main(String[] args) {
-
-        DicePlayer player = new DicePlayer();
-
-        player.setName("michael");
-        player.setCash(100);
-
-        System.out.println("Hello "+ player.getName());
-        System.out.printf(player.getName() + ", you have $%.2f on hand\n",player.getCash());
-
-
-        if(player.addCash(100)) //should work
-        {
-            System.out.println("Added 100 to cash");
-        }
-        else
-        {
-            System.out.println("failed to add 100 to cash");
-        }   
-
-        if(player.addCash(-100)) //should fail
-        {
-            System.out.println("Added -100 to cash");
-        }
-        else
-        {
-            System.out.println("failed to add -100 to cash");
-        }
-
-        System.out.printf(player.getName() + ", you have $%.2f on hand\n",player.getCash());
-
-
-        if (player.subtractCash(12)) //should work
-        {
-            System.out.println("Withdrawal of 12 OK");
-        } 
-        else
-        {
-            System.out.println("Withdrawal of 12 fail");
-        }
-
-        System.out.printf(player.getName() + ", you have $%.2f on hand\n",player.getCash());
-
-        if (player.subtractCash(300)) //should fail
-        {
-            System.out.println("Withdrawal of 300 OK");
-        } 
-        else
-        {
-            System.out.println("Withdrawal of 300 fail");
-        }
-
-         System.out.printf(player.getName() + ", you have $%.2f on hand\n",player.getCash());
-
-        if (player.subtractCash(-1)) //should fail
-        {
-            System.out.println("Withdrawal of -1 OK");
-        } 
-        else
-        {
-            System.out.println("Withdrawal of -1 fail");
-        }
-
-        System.out.printf(player.getName() + ", you have $%.2f on hand\n",player.getCash());
+    public static void main(String[] args)
+    {
+        
     }
-    
 }
